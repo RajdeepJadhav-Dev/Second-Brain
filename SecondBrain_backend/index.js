c34c77a64441f5bf7a6326bce87a63d79705a777
RajdeepJadhav-Dev <210rajdeep@gmail.com>
2025-01-25 13:57:00 +0530

    added cors

--- SecondBrain_backend/index.js
+++ SecondBrain_backend/index.js
@@ -11,11 +11,11 @@ const crudRouter = require('./routes/CRUD');
 const { preprocess } = require('zod');
 const PORT = process.env.PORT || 3000;
 
-/*app.use(cors({
+app.use(cors({
     origin: 'https://second-brain-frontend-drab.vercel.app', // Replace with your Vercel frontend URL
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify HTTP methods allowed
     credentials: true, // Allow cookies if needed
-  }));*/
+  }));
   app.use(cors());
 app.use('/',authrouter);
 app.use('/',crudRouter);
